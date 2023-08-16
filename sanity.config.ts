a94adf7856e6f2a5535313262811c5fb014f8ba0
jeanmax1me <jeanmaximegilbert@gmail.com>
2023-08-16 17:08:49 +0200

    Okay

---
+++ sanity.config.ts
@@ -0,0 +1,69 @@
+import { visionTool } from '@sanity/vision'
+import { defineConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
+import {
+  defineUrlResolver,
+  Iframe,
+  IframeOptions,
+} from 'sanity-plugin-iframe-pane'
+import { previewUrl } from 'sanity-plugin-iframe-pane/preview-url'
+
+import {
+  apiVersion,
+  dataset,
+  previewSecretId,
+  projectId,
+} from '~/lib/sanity.api'
+import { schema } from '~/schemas'
+
+// 1. Add the import to the theme.js you downloaded
+import {theme as _theme} from './theme'
+
+// 2. Assign typings to the theme
+const theme = _theme as import('sanity').StudioTheme
+
+
+const iframeOptions = {
+  url: defineUrlResolver({
+    base: '/api/draft',
+    requiresSlug: ['post'],
+  }),
+  urlSecretId: previewSecretId,
+  reload: { button: true },
+} satisfies IframeOptions
+
+export default defineConfig({
+  basePath: '/studio',
+  name: 'MA_Travel_Studio',
+  title: 'MA_Travel_Studio',
+  theme,
+  schema,
+  projectId: 've3fdxa5',
+  dataset,
+  plugins: [
+    deskTool({
+      // `defaultDocumentNode` is responsible for adding a “Preview” tab to the document pane
+      // You can add any React component to `S.view.component` and it will be rendered in the pane
+      // and have access to content in the form in real-time.
+      // It's part of the Studio's “Structure Builder API” and is documented here:
+      // https://www.sanity.io/docs/structure-builder-reference
+      defaultDocumentNode: (S, { schemaType }) => {
+        return S.document().views([
+          // Default form view
+          S.view.form(),
+          // Preview
+          S.view.component(Iframe).options(iframeOptions).title('Preview'),
+        ])
+      },
+    }),
+    // Add the "Open preview" action
+    previewUrl({
+      base: '/api/draft',
+      requiresSlug: ['post'],
+      urlSecretId: previewSecretId,
+    }),
+    // Vision lets you query your content with GROQ in the studio
+    // https://www.sanity.io/docs/the-vision-plugin
+    visionTool({ defaultApiVersion: apiVersion }),
+  ],
+})
