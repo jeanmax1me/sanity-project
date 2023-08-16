5403f2bc9a37557dab76e602d6e0a049b51dad55
jeanmax1me <jeanmaximegilbert@gmail.com>
2023-08-17 00:00:47 +0200

    mvp

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,11 +1,13 @@
+import Image from 'next/image'
 import Link from 'next/link'
+
 
 export default function Container({ children }: { children: React.ReactNode }) {
   return (
     <div className="container">
       <header className="header">
         <Link className="header__title" href="/">
-          Next.js + Sanity
+<h1 className="text-cyan-500 tracking-widest">Get Inspired For Your Next Getaway</h1>
         </Link>
       </header>
       <main>{children}</main>
@@ -22,12 +24,19 @@ export default function Container({ children }: { children: React.ReactNode }) {
           >
             <path
               d="M17 16C15.8 17.3235 12.5 20.5 12.5 20.5C12.5 20.5 9.2 17.3235 8 16C5.2 12.9118 4.5 11.7059 4.5 9.5C4.5 7.29412 6.1 5.5 8.5 5.5C10.5 5.5 11.7 6.82353 12.5 8.14706C13.3 6.82353 14.5 5.5 16.5 5.5C18.9 5.5 20.5 7.29412 20.5 9.5C20.5 11.7059 19.8 12.9118 17 16Z"
-              fill="currentColor"
+              fill="#ff69b4"
               stroke="currentColor"
               strokeWidth="1.2"
             ></path>
           </svg>{' '}
-          at Sanity
+          at 
+            <Link
+              target="_blank"
+              href="https://mariealicetravels.vercel.app"
+              className="font-bold ml-2 underline text-[#ff69b4]"
+            >
+              MarieAliceTravels
+            </Link>
         </p>
       </footer>
     </div>
